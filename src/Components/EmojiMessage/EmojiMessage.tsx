39acbbe4c518a3d132e5a8a7a0e85f5a3d370e3e
mostafa kheibary <mostafa.kheibary@gmail.com>
2022-08-18 19:12:52 +0430

    feat:add search and complete style of emoji sender

--- src/Components/EmojiMessage/EmojiMessage.tsx
+++ src/Components/EmojiMessage/EmojiMessage.tsx
@@ -1,10 +1,15 @@
-import React, { FC, ReactNode, useMemo, useState } from 'react';
+import { ChangeEvent, FC, ReactNode, useMemo, useState } from 'react';
 import emojisData from '../../data/emojies.json';
 
-import { IconButton } from '@mui/material';
+import { IconButton, Input } from '@mui/material';
 import EmojiEmotionsIcon from '@mui/icons-material/EmojiEmotions';
 import TagFacesIcon from '@mui/icons-material/TagFaces';
 import ThumbUpAltIcon from '@mui/icons-material/ThumbUpAlt';
+import PetsIcon from '@mui/icons-material/Pets';
+import FastfoodIcon from '@mui/icons-material/Fastfood';
+import SportsHandballIcon from '@mui/icons-material/SportsHandball';
+import EmojiFlagsIcon from '@mui/icons-material/EmojiFlags';
+import EmojiObjectsIcon from '@mui/icons-material/EmojiObjects';
 import classNames from '../../utils/classNames';
 import './EmojiMessage.css';
 import { useDispatch } from 'react-redux';
@@ -28,18 +33,20 @@ interface ICatagory {
 const EmojiMessage: FC = () => {
   const dispatch = useDispatch();
   const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [searchValue, setSearchValue] = useState<string>('');
   const [currentCatagory, setCurrentCatagory] = useState<emojiType>('Smileys & Emotion');
-  const [emojis, setEmojis] = useState<any>([]);
+  const [emojis, setEmojis] = useState<any[]>([]);
+  const [filterdEmoji, setFilterdEmoji] = useState<any[]>([]);
 
   const catagorys: ICatagory[] = [
     { name: 'Smileys & Emotion', icon: <TagFacesIcon /> },
     { name: 'People & Body', icon: <ThumbUpAltIcon /> },
-    { name: 'Animals & Nature', icon: <ThumbUpAltIcon /> },
+    { name: 'Animals & Nature', icon: <PetsIcon /> },
     { name: 'Food & Drink', icon: <ThumbUpAltIcon /> },
-    { name: 'Travel & Places', icon: <ThumbUpAltIcon /> },
-    { name: 'Activities', icon: <ThumbUpAltIcon /> },
-    { name: 'Objects', icon: <ThumbUpAltIcon /> },
-    { name: 'Flags', icon: <ThumbUpAltIcon /> },
+    { name: 'Travel & Places', icon: <FastfoodIcon /> },
+    { name: 'Activities', icon: <SportsHandballIcon /> },
+    { name: 'Objects', icon: <EmojiObjectsIcon /> },
+    { name: 'Flags', icon: <EmojiFlagsIcon /> },
   ];
   const handleOpenTab = (): void => {
     setIsOpen(true);
@@ -50,17 +57,26 @@ const EmojiMessage: FC = () => {
   const addEmoji = (emoji: string) => {
     dispatch(addMessageInput(emoji));
   };
+  const handleSearchEmoji = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(e.target.value);
+    const searchTerm = e.target.value.trim();
+    const filterd = emojis.filter((emoji) => emoji.description.includes(searchTerm));
+    console.log(filterd);
+    setFilterdEmoji(filterd);
+  };
   useMemo(() => {
-    setEmojis(emojisData.filter((emoji) => emoji.category === currentCatagory));
+    const filterd = emojisData.filter((emoji) => emoji.category === currentCatagory);
+    setEmojis(filterd);
+    setFilterdEmoji(filterd);
   }, [currentCatagory]);
 
   const renderedEmojis = useMemo(() => {
-    return emojis.map((emoji: any, i: number) => (
+    return filterdEmoji.map((emoji: any, i: number) => (
       <span className='emoji__select-tab__emoji' key={i} onClick={() => addEmoji(emoji.emoji)}>
         {emoji.emoji}
       </span>
     ));
-  }, [emojis]);
+  }, [filterdEmoji]);
 
   return (
     <div onMouseLeave={handleCloseTab} className='emoji-sender'>
@@ -68,7 +84,17 @@ const EmojiMessage: FC = () => {
         <EmojiEmotionsIcon />
       </IconButton>
       <div className={classNames('emoji__select-tab', isOpen ? 'open' : '')}>
-        <div className='emoji__select-tab__head'>
+        <div className='emoji__select-tab__search-head'>
+          <Input
+            value={searchValue}
+            onChange={handleSearchEmoji}
+            placeholder='Search Emoji'
+            className='emoji__select-tab__search-input'
+          />
+        </div>
+        <div className='emoji__select-tab__wrapper'>{renderedEmojis}</div>
+        {filterdEmoji.length <= 0 && <h4 className='emoji__select__not-found'>Emoji Not Found</h4>}
+        <div className='emoji__select-tab__bottom'>
           {catagorys.map((catagory, i) => (
             <IconButton
               color={catagory.name === currentCatagory ? 'primary' : 'default'}
@@ -80,7 +106,6 @@ const EmojiMessage: FC = () => {
             </IconButton>
           ))}
         </div>
-        <div className='emoji__select-tab__wrapper'>{renderedEmojis}</div>
       </div>
     </div>
   );
